b0e840d167d94f445629ce39a58bd53e959d7201
xyyxhcj <xyyxhcj@qq.com>
2020-02-24 16:15:31 +0800

    optimize css

--- front/src/views/api/edit.vue
+++ front/src/views/api/edit.vue
@@ -2,15 +2,33 @@
     <div id="api-edit-container">
         <el-form :model="form" label-position="left" label-width="80px" ref="form" :rules="form_rules">
             <el-form-item label="apiStatus" size="mini" style="text-align: left">
-                <el-radio v-model.trim="form.apiStatus" :label="0" size="mini">{{CONSTANT.API_STATUS[0]}}</el-radio>
-                <el-radio v-model.trim="form.apiStatus" :label="1" size="mini">{{CONSTANT.API_STATUS[1]}}</el-radio>
-                <el-radio v-model.trim="form.apiStatus" :label="2" size="mini">{{CONSTANT.API_STATUS[2]}}</el-radio>
-                <el-radio v-model.trim="form.apiStatus" :label="3" size="mini">{{CONSTANT.API_STATUS[3]}}</el-radio>
-                <el-radio v-model.trim="form.apiStatus" :label="4" size="mini">{{CONSTANT.API_STATUS[4]}}</el-radio>
-                <el-radio v-model.trim="form.apiStatus" :label="5" size="mini">{{CONSTANT.API_STATUS[5]}}</el-radio>
-                <el-radio v-model.trim="form.apiStatus" :label="6" size="mini">{{CONSTANT.API_STATUS[6]}}</el-radio>
-                <el-radio v-model.trim="form.apiStatus" :label="7" size="mini">{{CONSTANT.API_STATUS[7]}}</el-radio>
-                <el-radio v-model.trim="form.apiStatus" :label="8" size="mini">{{CONSTANT.API_STATUS[8]}}</el-radio>
+                <el-radio v-model.trim="form.apiStatus" :label="0" size="mini" class="success">
+                    {{CONSTANT.API_STATUS[0]}}
+                </el-radio>
+                <el-radio v-model.trim="form.apiStatus" :label="7" size="mini" class="warning">
+                    {{CONSTANT.API_STATUS[7]}}
+                </el-radio>
+                <el-radio v-model.trim="form.apiStatus" :label="6" size="mini" class="warning">
+                    {{CONSTANT.API_STATUS[6]}}
+                </el-radio>
+                <el-radio v-model.trim="form.apiStatus" :label="1" size="mini" class="warning">
+                    {{CONSTANT.API_STATUS[1]}}
+                </el-radio>
+                <el-radio v-model.trim="form.apiStatus" :label="5" size="mini" class="warning">
+                    {{CONSTANT.API_STATUS[5]}}
+                </el-radio>
+                <el-radio v-model.trim="form.apiStatus" :label="4" size="mini" class="warning">
+                    {{CONSTANT.API_STATUS[4]}}
+                </el-radio>
+                <el-radio v-model.trim="form.apiStatus" :label="3" size="mini" class="warning">
+                    {{CONSTANT.API_STATUS[3]}}
+                </el-radio>
+                <el-radio v-model.trim="form.apiStatus" :label="8" size="mini" class="error">
+                    {{CONSTANT.API_STATUS[8]}}
+                </el-radio>
+                <el-radio v-model.trim="form.apiStatus" :label="2" size="mini" class="error">
+                    {{CONSTANT.API_STATUS[2]}}
+                </el-radio>
             </el-form-item>
             <el-form-item label="path" size="mini">
                 <el-select v-model.trim="form.apiRequestType" style="width: 8%">
@@ -295,5 +313,29 @@
     };
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus" scoped>
+<style lang="stylus" rel="stylesheet/stylus">
+    #api-edit-container
+        .success
+            color #44B549
+
+            &.is-checked
+                .el-radio__label
+                    color #44B549
+                    font-weight bold
+
+        .warning
+            color #E6A23C
+
+            &.is-checked
+                .el-radio__label
+                    font-weight bold
+                    color #E6A23C
+
+        .error
+            color #F56C6C
+
+            &.is-checked
+                .el-radio__label
+                    font-weight bold
+                    color #F56C6C
 </style>
