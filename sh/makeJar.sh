a3172835a99e5b7f43d8df15aeaba7679b2e770e
xyyxhcj <xyyxhcj@qq.com>
2020-05-27 17:28:00 +0800

    chore: timeout-per-shutdown-phase

--- sh/makeJar.sh
+++ sh/makeJar.sh
@@ -59,9 +59,11 @@ cd ${projectDir}ams && mvn clean package -DskipTests
 
 # close old
 appPid=$(netstat -ntlp | grep 11111 | awk '{print $7}' | head -1 | grep '[0-9]\+' -o)
-kill -2 "${appPid}"
-echo "killed ${appPid}"
-sleep 15s
+if  [ "$appPid" != "" ] ;then
+    kill -2 "${appPid}"
+    echo "killed ${appPid}"
+    sleep 15s
+fi
 
 # move to nginx file & start
 cp ${projectDir}front/dist/index.html ${nginxHtmlDir}
