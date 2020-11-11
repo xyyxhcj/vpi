c853347cf653e87acafd23adc965cf71d1bd048f
xyyxhcj@qq.com <xyyxhcj@qq.com>
2020-11-11 16:40:02 +0800

    style: edit help & sh

--- README_zh_CN.md
+++ README_zh_CN.md
@@ -1,3 +1,4 @@
+此为前后端分离项目
 使用  **sh/makeJar.sh**进行项目配置,启动!
 ## 配置文件:
 front/vue.config.js  
@@ -6,8 +7,11 @@ ams/src/main/java/press/whcj/ams/common.SysConfig
 ams/src/main/resources/application.yml  
 ams/src/main/resources/application-dev.yml  
 ## 需要安装: 
+jdk1.8
 chrome插件,  
-mongodb(init/install-mongodb.sh)
+mongodb(init/install-mongodb.sh),
+nginx
+#如果使用sh/makeJar.sh，需要安装git,maven,node
 ## 演示:
 http://www.whcj.press  
 测试账号: lx/123456
--- sh/mongodb_bak.sh
+++ sh/mongodb_bak.sh
@@ -13,12 +13,12 @@ DATE=$(date -d 'today' +'%Y-%m-%d-%H-%M-%S')
 EXPIRE_DAYS=7
 FILE_NAME="mongodb_bak_${DATE}.tar.gz"
 rm -rf ${OUT_DIR}
-mkdir -p ${OUT_DIR}/${DATE}
+mkdir -p "${OUT_DIR}/${DATE}"
 mkdir -p ${SAVE_DIR}
 # save
-$DUMP -h ${HOST} -d ${DB_NAME} -o ${OUT_DIR}/${DATE} -u ${USER} -p ${PASSWORD}
+$DUMP -h ${HOST} -d ${DB_NAME} -o "${OUT_DIR}/${DATE}" -u ${USER} -p ${PASSWORD}
 # save to .tar.gz
-tar -zcvPf ${SAVE_DIR}/${FILE_NAME} ${OUT_DIR}/${DATE}
+tar -zcvPf "${SAVE_DIR}/${FILE_NAME}" "${OUT_DIR}/${DATE}"
 # remove expire file
 find ${SAVE_DIR}/ -mtime +${EXPIRE_DAYS} -delete
 # send to other server
