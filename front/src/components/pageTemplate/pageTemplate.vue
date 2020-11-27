e6fab764f13c525d61d707eeaa7c1d76e95feca3
hcj <xyyxhcj@qq.com>
2020-11-27 15:02:06 +0800

    style:分页组件优化

--- front/src/components/pageTemplate/pageTemplate.vue
+++ front/src/components/pageTemplate/pageTemplate.vue
@@ -1,41 +1,46 @@
 <template>
-    <div class="container">
-    <el-button icon="el-icon-arrow-left" style="padding:10px" @click="prevEvent" :disabled="this.query.page.current == 1 ? true:false" ></el-button>
-     {{this.query.page.current}}
-    <el-button icon="el-icon-arrow-right" style="padding:10px" @click="nextEvent" :disabled="this.query.page.total < 20 ? true : false"></el-button>
-    </div>
+  <div class="container">
+    <el-button class="page-content" icon="el-icon-arrow-left" @click="prevEvent" size="mini"
+               :disabled="query.page.current === 1"></el-button>
+    <span class="page-content">{{ this.query.page.current }}</span>
+    <el-button class="page-content" icon="el-icon-arrow-right" @click="nextEvent" size="mini"
+               :disabled="query.page.total < size"></el-button>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    import {CONSTANT} from "../../common/js/constant";
-    export default {
+import {CONSTANT} from "../../common/js/constant";
+
+export default {
         name: 'pageTemplate',
         props: {
-            query: Object
+          query: Object
         },
         data() {
-            return {
+          return {
             CONSTANT: CONSTANT,
             total: 0,
             size: 20,
             currentPage: 1
-            }
+          }
         },
         methods: {
-            prevEvent(){
-            this.query.page.current = this.query.page.current-1;
+          prevEvent() {
+            this.query.page.current = this.query.page.current - 1;
             this.$emit('flush');
-            },
-            nextEvent(){
-            this.query.page.current = this.query.page.current+1;
+          },
+          nextEvent() {
+            this.query.page.current = this.query.page.current + 1;
             this.$emit('flush');
-            },
+          },
         }
     };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-    .container
-        padding: 10px
-        background-color: #fff
+.container
+  padding 7px
+  background-color #fff
+  .page-content
+    padding 7px
 </style>
